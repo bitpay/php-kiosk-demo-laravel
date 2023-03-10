71e95c3a3cf60ee6fc77199c6414cc0f33a722a6
Marcin Warzybok <m.warzybok@sumoheavy.com>
2023-03-10 10:06:54 +0100

    SP-435 & SP-461 & SP-470

---
+++ app/Http/Controllers/Invoice/CreateInvoiceController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Invoice;
+
+use App\Features\Invoice\CreateInvoice;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\HttpFoundation\Request;
+
+class CreateInvoiceController extends Controller
+{
+    private CreateInvoice $createInvoice;
+
+    public function __construct(CreateInvoice $createInvoice)
+    {
+        $this->createInvoice = $createInvoice;
+    }
+
+    /**
+     * @throws \BitpaySDK\Exceptions\BitPayException
+     * @throws \JsonException
+     */
+    public function execute(Request $request): RedirectResponse
+    {
+        $invoice = $this->createInvoice->execute($request->request->all());
+
+        return Redirect::to($invoice->bitpay_url);
+    }
+}
